bf93f8a2b139c18271228b0b243aea0f82d0bc59
DAVID MOORE <khundawg@gmail.com>
2014-07-27 00:11:10 -0400

    Ditch env variables for CreateAdminService

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,9 +1,9 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-    		user.name = Rails.application.secrets.admin_name
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+    user = User.find_or_create_by!(email: 'khundawg@gmail.com') do |user|
+    		user.name = 'Dave Moore'
+        user.password = 'changeme'
+        user.password_confirmation = 'changeme'
         user.admin!
       end
   end
