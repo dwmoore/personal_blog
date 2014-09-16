ae47030f7a7905aef06eaaef9b6f5a4aae56ef46
DAVID MOORE <khundawg@gmail.com>
2014-09-16 11:32:08 -0400

    Removed the ability to signup in production.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+
+  if Rails.env.production?
+    devise_for :users, :controllers => { :registrations => "registrations" } 
+  else
+    devise_for :users
+  end
+
   resources :users, :posts
 end
